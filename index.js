5f670f2b4250aa914959ccd061490f0c866d640a
Federico Brigante <github@bfred.it>
2019-03-03 00:32:21 +0800

    Initial version

---
+++ index.js
@@ -0,0 +1,101 @@
+const createPrivateKey = Symbol('createPrivateKey');
+const getPrivateKey = Symbol('getPrivateKey');
+const publicKeys = Symbol('publicKeys');
+const objectHashes = Symbol('objectHashes');
+
+let keyCounter = 0;
+
+export default class MultiMap extends Map {
+	constructor(pairs) {
+		super();
+		this[objectHashes] = new WeakMap();
+		this[publicKeys] = new Map();
+
+		if (pairs === null || pairs === undefined) {
+			return;
+		}
+
+		if (typeof pairs[Symbol.iterator] !== 'function') {
+			throw new TypeError(typeof pairs + ' is not iterable (cannot read property Symbol(Symbol.iterator))');
+		}
+
+		for (const [keys, value] of pairs) {
+			this.set(keys, value);
+		}
+	}
+
+	[createPrivateKey](keys) {
+		return JSON.stringify(keys.map(key => {
+			if (typeof key === 'object') {
+				if (this[objectHashes].has(key)) {
+					return this[objectHashes].get(key);
+				}
+
+				keyCounter++;
+				this[objectHashes].set(key, keyCounter);
+				return keyCounter;
+			}
+
+			return key;
+		}));
+	}
+
+	[getPrivateKey](keys) {
+		return JSON.stringify(keys.map(key => {
+			if (typeof key === 'object') {
+				if (this[objectHashes].has(key)) {
+					return this[objectHashes].get(key);
+				}
+
+				// Impossible key
+				return Math.random() + '.' + Math.random();
+			}
+
+			return key;
+		}));
+	}
+
+	set(keys, value) {
+		if (!Array.isArray(keys)) {
+			throw new TypeError('The keys parameter must be an array');
+		}
+
+		const privateKey = this[createPrivateKey](keys);
+
+		let publicKey;
+		if (this[publicKeys].has(privateKey)) {
+			publicKey = this[publicKeys].get(privateKey);
+		} else {
+			this[publicKeys].set(privateKey, keys);
+			publicKey = keys;
+		}
+
+		return super.set(publicKey, value);
+	}
+
+	get(keys) {
+		if (!Array.isArray(keys)) {
+			throw new TypeError('The keys parameter must be an array');
+		}
+
+		return super.get(this[publicKeys].get(this[getPrivateKey](keys)));
+	}
+
+	has(keys) {
+		if (!Array.isArray(keys)) {
+			throw new TypeError('The keys parameter must be an array');
+		}
+
+		return super.has(this[publicKeys].get(this[getPrivateKey](keys)));
+	}
+
+	delete(keys) {
+		if (!Array.isArray(keys)) {
+			throw new TypeError('The keys parameter must be an array');
+		}
+
+		const privateKey = this[getPrivateKey](keys);
+		const publicKey = this[publicKeys].get(privateKey);
+		return Boolean(publicKey && super.delete(publicKey) && this[publicKeys].delete(privateKey));
+	}
+}
---
+++ test.js
@@ -0,0 +1,172 @@
+import test from 'ava';
+import MultiMap from '.';
+
+test('Basics', t => {
+	const map = new MultiMap();
+	t.true(map instanceof Map);
+	t.is(typeof map[Symbol.iterator], 'function');
+	t.is(map.size, 0);
+	t.is(map.get.length, 1);
+	t.is(map.set.length, 2);
+	t.is(map.clear.length, 0);
+	t.is(map.delete.length, 1);
+	t.deepEqual([...map.entries()], []);
+	t.deepEqual([...map.values()], []);
+	t.deepEqual([...map.keys()], []);
+	map.forEach(t.fail);
+});
+
+test('Set', t => {
+	const map = new MultiMap();
+	map.set(['-'], 'first');
+	t.is(map.size, 1);
+	map.set(['-'], 'second');
+	t.is(map.size, 1);
+	map.set([':', '-'], 'third');
+	t.is(map.size, 2);
+	map.set([':', '-', '%'], 'fourth');
+	t.is(map.size, 3);
+
+	// Also make sure that the same map is returned
+	t.is(map.set(['#', 'fifth']), map);
+
+	const prefilledMap = new MultiMap([
+		[['-'], 'first'],
+		[[':', '-'], 'second']
+	]);
+	t.is(prefilledMap.size, 2);
+});
+
+test('Get', t => {
+	const map = new MultiMap([
+		[['-'], 'first'],
+		[[':', '-'], 'second'],
+		[[':', '-', '%'], 'third']
+	]);
+
+	t.is(map.get(['-']), 'first');
+	t.is(map.get([':', '-']), 'second');
+	t.is(map.get([':', '-', '%']), 'third');
+	t.is(map.get([':']), undefined);
+	t.is(map.get([':', '%']), undefined);
+	t.is(map.get([':', '%', '-']), undefined);
+});
+
+test('Has', t => {
+	const map = new MultiMap([
+		[['-'], 'first'],
+		[[':', '-'], 'second'],
+		[[':', '-', '%'], 'third']
+	]);
+
+	t.true(map.has(['-']));
+	t.true(map.has([':', '-']));
+	t.true(map.has([':', '-', '%']));
+	t.false(map.has([':']));
+	t.false(map.has([':', '%']));
+	t.false(map.has([':', '%', '-']));
+});
+
+test('Delete', t => {
+	const map = new MultiMap([
+		[['-'], 'first'],
+		[[':', '-'], 'second'],
+		[[':', '-', '%'], 'third']
+	]);
+
+	t.is(map.size, 3);
+	t.true(map.delete(['-']));
+	t.is(map.size, 2);
+	t.false(map.delete(['-']));
+	t.is(map.size, 2);
+	t.true(map.delete([':', '-']));
+	t.is(map.size, 1);
+	t.true(map.delete([':', '-', '%']));
+	t.is(map.size, 0);
+});
+
+test('Clear', t => {
+	const map = new MultiMap([
+		[['-'], 'first'],
+		[[':', '-'], 'second'],
+		[[':', '-', '%'], 'third']
+	]);
+
+	t.is(map.size, 3);
+	t.is(map.clear(), undefined);
+	t.is(map.size, 0);
+	t.is(map.clear(), undefined);
+	t.is(map.size, 0);
+});
+
+test('Iterators', t => {
+	const pairs = [
+		[['-'], 'first'],
+		[[':', '-'], 'second'],
+		[[':', '-', '%'], 'third']
+	];
+	const map = new MultiMap(pairs);
+	const regularMap = new Map(pairs);
+
+	t.deepEqual([...map], pairs);
+	t.deepEqual([...map.entries()], pairs);
+
+	// The returned values and keys match regular Maps,
+	// but in MultiMap, key Arrays are stored by value rather than by reference.
+	t.deepEqual([...map.values()], [...regularMap.values()]);
+	t.deepEqual([...map.keys()], [...regularMap.keys()]);
+
+	let count = 0;
+	map.forEach(() => {
+		count++;
+	});
+	t.is(count, pairs.length);
+});
+
+test('All types of keys', t => {
+	const map = new MultiMap();
+
+	t.is(map.set([1], 'number').get([1]), 'number');
+	t.is(map.size, 1);
+	t.true(map.delete([1]));
+	t.is(map.size, 0);
+
+	t.is(map.set([true], 'boolean').get([true]), 'boolean');
+	t.is(map.size, 1);
+	t.true(map.delete([true]));
+	t.is(map.size, 0);
+
+	t.is(map.set([undefined], 'undefined').get([undefined]), 'undefined');
+	t.is(map.size, 1);
+	t.true(map.delete([undefined]));
+	t.is(map.size, 0);
+
+	let key = {};
+	t.is(map.set([key], 'object').get([key]), 'object');
+	t.is(map.size, 1);
+	t.true(map.delete([key]));
+	t.is(map.size, 0);
+
+	key = [];
+	t.is(map.set([key], 'array').get([key]), 'array');
+	t.is(map.size, 1);
+	t.true(map.delete([key]));
+	t.is(map.size, 0);
+});
+
+test('Mixed types of keys', t => {
+	const map = new MultiMap();
+	map.set([1, '1', true], 'truthy');
+	t.is(map.size, 1);
+	t.is(map.get([1, '1', true]), 'truthy');
+	t.is(map.get([1, '1', 'true']), undefined);
+	t.is(map.get(['1', '1', true]), undefined);
+	t.is(map.get([1, '1', true, 1]), undefined);
+	t.is(map.get([1, 1, 1]), undefined);
+
+	map.set([undefined], 'undefined');
+	t.is(map.size, 3);
+	t.is(map.get([undefined]), 'undefined');
+	t.is(map.get(['undefined']), undefined);
+	t.is(map.get([,]), 'undefined'); // eslint-disable-line no-sparse-arrays,comma-spacing
+});
