f1babb1a9e6343adf6341c1e4753086c3092ae38
mshriner <mshriner@users.noreply.github.com>
2025-06-03 21:24:20 -0500

    Update index.d.ts with default keyword
    Co-authored-by: fregante <me@fregante.com>

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/ban-types -- It matches Map’s
-export class ManyKeysMap<K, V> extends Map<K[], V> {}
+export default class ManyKeysMap<K, V> extends Map<K[], V> {}
