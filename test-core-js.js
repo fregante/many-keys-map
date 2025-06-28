b08708f3b15162c18a388b112fc4f219cac77ebe
mshriner <marshriner@gmail.com>
2025-06-28 16:17:56 -0500

    fix quotation marks in copyright notice

--- test-core-js.js
+++ test-core-js.js
@@ -4,7 +4,7 @@
 
 Copyright(c) 2014 - 2019 Denis Pushkarev
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
@@ -12,7 +12,7 @@ furnished to do so, subject to the following conditions:
 	The above copyright notice and this permission notice shall be included in
 	all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -365,7 +365,7 @@ describe('ManyKeysMap#size', () => {
 	it('functionality', () => {
 		const map = new ManyKeysMap();
 		map.set([2], 1);
-		const {size} = map;
+		const { size } = map;
 		expect(typeof size).toBe('number');
 		expect(size).toBe(1);
 		const sizeDescriptor = Object.getOwnPropertyDescriptor(
